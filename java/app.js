ee202c6ee5c80daf1e0866f31105b7f264df496a
mohammad mahmoud mohammad hassouneh <mohammad.hs91@hotmail.com>
2021-02-23 01:51:05 +0200

    lab12

--- java/app.js
+++ java/app.js
@@ -12,8 +12,12 @@ let userAttemtesCounter = 0;
 let leftImgIndex;
 let centerImgIndex;
 let rightImgIndex;
+let ShopName = [];
+let ShopVotes = [];
+let ShopShow = [];
+let indexs = [];
 
-function ShopImg(name, source , show) {
+function ShopImg(name, source) {
     this.name = name;
     this.source = source;
     this.votes = 0;
@@ -41,7 +45,7 @@ new ShopImg('tauntaun', 'img/tauntaun.jpg');
 new ShopImg('unicorn', 'img/unicorn.jpg');
 new ShopImg('usb', 'img/usb.gif');
 
-
+console.log(ShopImg.allImg);
 // console.log(ShopImg.allImg[0]['name']);
 // console.log(ShopImg.allImg[0].votes);
 
@@ -50,36 +54,43 @@ function generateRandomIndex() {
 
 }
 
-    function renderThreeImg() {
-        leftImgIndex = generateRandomIndex();
+function renderThreeImg() {
+    leftImgIndex = generateRandomIndex();
+    // centerImgIndex = generateRandomIndex();
+    // rightImgIndex = generateRandomIndex();
+
+
+    do {
         centerImgIndex = generateRandomIndex();
+
+    } while (leftImgIndex === centerImgIndex)
+
+    do {
         rightImgIndex = generateRandomIndex();
 
+    } while (leftImgIndex === rightImgIndex || centerImgIndex === rightImgIndex)
 
-        do {
-            rightImgIndex = generateRandomIndex();
-        } while (leftImgIndex === rightImgIndex)
+    indexs.push(leftImgIndex, centerImgIndex, rightImgIndex);
 
-        ShopImg.allImg
+    // ShopImg.allImg
 
-        console.log(ShopImg.allImg[leftImgIndex]);
+    console.log(ShopImg.allImg[leftImgIndex]);
 
-        leftImgElement.src = ShopImg.allImg[leftImgIndex].source;
-        ShopImg.allImg[leftImgIndex].show++
-        centerImgElmenet.src = ShopImg.allImg[centerImgIndex].source;
-        ShopImg.allImg[centerImgIndex].show++
+    leftImgElement.src = ShopImg.allImg[leftImgIndex].source;
+    ShopImg.allImg[leftImgIndex].show++
+    centerImgElmenet.src = ShopImg.allImg[centerImgIndex].source;
+    ShopImg.allImg[centerImgIndex].show++
 
-        rightImgElmenet.src = ShopImg.allImg[rightImgIndex].source;
-        ShopImg.allImg[rightImgIndex].show++
+    rightImgElmenet.src = ShopImg.allImg[rightImgIndex].source;
+    ShopImg.allImg[rightImgIndex].show++
+}
 
-
-    }
 renderThreeImg();
 leftImgElement.addEventListener('click', handleUserClick);
 centerImgElmenet.addEventListener('click', handleUserClick);
 rightImgElmenet.addEventListener('click', handleUserClick);
 // ShopResult.addEventListener('results-list');
-myBtn.addEventListener('click',showList)
+myBtn.addEventListener('click', showList);
 
 function handleUserClick(event) {
     userAttemtesCounter++;
@@ -95,51 +106,95 @@ function handleUserClick(event) {
 
         else { ShopImg.allImg[rightImgIndex].votes++ }
 
-        renderThreeImg();
+        do{ 
+            renderThreeImg();
+            console.log(indexs);
+        }while (indexs.includes(leftImgIndex) || indexs.includes(centerImgIndex) || indexs.includes(rightImgIndex))
+         indexs = [];
+         console.log(indexes);
 
+       
     }
+
     else {
-        // show the list
-
-
-    //    let list getElementById(); 
-    //     let ShopResult; 
-    //     for (let i = 0; i < ShopImg.allImg.length; i++) {
-    //         ShopResult = document.createElement('li');
-    //         list.appendChild(ShopResult);
-    //         ShopResult.textContent = ShopImg.allImg[i].name + 'have' + ShopImg.allImg[i].votes + 'votes';
-
-    //     }
-
-        
         leftImgElement.removeEventListener('click', handleUserClick);
         centerImgElmenet.removeEventListener('click', handleUserClick);
         rightImgElmenet.removeEventListener('click', handleUserClick);
-        
-        
+        myBtn.addEventListener('click', showList);
+
+        for (let i = 0; i < ShopImg.allImg.length; i++) {
+            // 0
+            ShopName.push(ShopImg.allImg[i].name);
+
+            ShopVotes.push(ShopImg.allImg[i].votes);
+
+            ShopShow.push(ShopImg.allImg[i].show);
+
+        }
+        viewChart();
     }
 }
 
 // making a show list function:
-function showList(event){
+function showList(event) {
     let list = document.getElementById('results-list');
     console.log(list);
 
-    for(let i = 0; i < ShopImg.allImg.length; i++){
-        
-    //   console.log( ShopImg.allImg[i]); 
-    // 0
-    // 1
-    // 2
-    
-    let resultsItem =  document.createElement('li');
-    list.appendChild(resultsItem);
-    // banana had 3 votes, and was seen 5 times.
+    for (let i = 0; i < ShopImg.allImg.length; i++) {
+
+        //   console.log( ShopImg.allImg[i]); 
+        // 0
+        // 1
+        // 2
+
+        let resultsItem = document.createElement('li');
+        list.appendChild(resultsItem);
+        // banana had 3 votes, and was seen 5 times.
         // console.log(ShopImg.allImg[0].votes);
-        resultsItem.textContent = ShopImg.allImg[i].name + ' had '+ ShopImg.allImg[i].votes +' votes, was seen ' + ShopImg.allImg[i].show  
+        resultsItem.textContent = ShopImg.allImg[i].name + ' had ' + ShopImg.allImg[i].votes + ' votes, was seen ' + ShopImg.allImg[i].show
     }
-    
-    myBtn.removeEventListener('click',showList);
+
+    myBtn.removeEventListener('click', showList);
+
+
+
+    function viewChart() {
+
+        let ctx = document.getElementById('myChart').getContext('2d');
+
+        let chart = new Chart(ctx, {
+            type: 'bar',
+
+            data: {
+                labels: showList,
+
+                datasets: [
+
+
+                    {
+                        label: 'shop votes',
+                        backgroundColor: '#1e212d',
+                        borderColor: '#1e212d',
+                        data: ShopVotes
+                    },
+
+                    {
+                        label: 'shop show',
+                        backgroundColor: 'blue',
+                        borderColor: 'blue',
+                        data: ShopShow
+                    },
+
+
+                ]
+
+            },
+            options:{}
+        });
+    }
+//       // Configuration options go here
+//       options: {}
+//     });
 
 }
 
